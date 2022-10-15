d30256405ca952d536d3178b277edc8f0b74067c
adioppo <115898990+adioppo@users.noreply.github.com>
2022-10-15 23:27:40 +0300

    Update beta.js

--- beta.js
+++ beta.js
@@ -10,9 +10,9 @@ document.getElementById("c").innerHTML = "gamma1 = "+gamma;
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 var centru = {x:canvas.width/2, y:canvas.height/2}
-var latura_patrat = 180;
+var latura_patrat = 50;
 context.clearRect(0, 0, canvas.width, canvas.height);
 context.fillStyle = "#6A0888";
 context.beginPath();
-context.fillRect(180, 150, evt.beta, 90);
+context.fillRect(180, 150, evt.beta, 50);
 }
